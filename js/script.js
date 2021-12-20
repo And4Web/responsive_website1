c2232005b731da2bb87dde1c61845b34fa13d025
Anand Shukla <anands2807@gmail.com>
2021-12-20 21:50:25 +0530

    search btn transition added.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let searchBtn = document.querySelector('#search-btn');
+
+let searchForm = document.querySelector('.header .search-box'); 
+
+searchBtn.onclick = () => {
+  searchBtn.classList.toggle('fa-times');
+  searchForm.classList.toggle('active');
+}
